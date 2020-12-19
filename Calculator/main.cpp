958f2404bee12c679a632e8602c153a1b6c32be4
Nawaf <58915468+nawafalqari@users.noreply.github.com>
2020-12-19 20:54:07 +0300

    Add files via upload

---
+++ Calculator/main.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int firstNum;
+    
+    int secondNum;
+
+    cout << "Enter first number" << endl;
+
+    cin >> firstNum;
+
+    cout << "Enter second number" << endl;
+
+    cin >> secondNum;
+
+    int soul = firstNum + secondNum;
+
+    cout << "\nThe answer is " << soul << endl;
+
+}
