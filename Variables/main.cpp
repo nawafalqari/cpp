8e154a5de5f848de5e9f87d123d94019d21918da
Nawaf <58915468+nawafalqari@users.noreply.github.com>
2021-01-01 05:32:17 +0300

    Variables explanation
    :)

---
+++ Variables/main.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+
+    int integer = 15; // stores whole numbers, without decimals 
+    string text = "text"; // stores text
+    double num = 3.22; // stores numbers with decimals 
+    bool value = true; // stores two states of values: true or false.
+    char charecter = 'A'; // stores single characters
+    float num2 = 1.314; // float is same as double but double handels bigger numbers
+
+    return 0;
+}
