a011aea8c1abe5e48ab4fc7f8aca5c48f5738ad2
Nawaf <58915468+nawafalqari@users.noreply.github.com>
2021-01-01 10:55:10 +0300

    While explanation
    :)

---
+++ while/main,.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    
+    int counter = 1;
+
+    while(counter <= 5) {
+        cout << counter << endl;
+        counter++;
+    }
+
+    return 0;
+}
+
+/*
+Result:
+
+1
+2
+3
+4
+5
+
+*/
