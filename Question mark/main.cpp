37a73b1afdf8f15a1d0cc8458c47a37ef5b3602f
Nawaf <58915468+nawafalqari@users.noreply.github.com>
2021-01-01 10:27:29 +0300

    If as question mark
    :)

---
+++ Question mark/main.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int input;
+
+    cout << "Enter any number... " << endl;
+    cin >> input;
+
+    input >= 50 ? cout << "Yay the number is bigger than 50" << endl : cout << "Ohh the number is samller than 50 sorry";
+
+    return 0;
+}
